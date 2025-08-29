da7a84e14242cab34518c071d003c7559c5f455c
Kenrawr <20102188@usc.edu.ph>
2025-08-29 23:43:29 +0800

    Stack v2

--- C/Stack/Stackv2.c
+++ C/Stack/Stackv2.c
@@ -27,3 +27,34 @@ void push(Stack*S,int data){
     }
 }
 
+int pop(Stack*S){
+    int retVal = -1;
+    return (!isEmpty(S)) ? (S->data[++S->top]) : retVal;
+}
+
+void display(Stack*S){
+    if(!isEmpty(S)) {
+        for(int i = S->top + 1; i < MAX; i++) {
+            printf("%d ", S->data[i]);
+        }
+        printf("\n");
+    } else {
+        printf("Stack is empty\n");
+    }
+}
+
+void main(){
+    Stack myStack;
+    init(&myStack);
+    printf("Stack initialized with top at %d\n", myStack.top);
+    push(&myStack, 10);
+    push(&myStack, 20);
+    push(&myStack, 30);
+      display(&myStack);
+    printf("%d popped from stack\n", pop(&myStack));
+    printf("%d popped from stack\n", pop(&myStack));
+    printf("Stack after pop operation: ");
+    display(&myStack);
+    
+}
+
