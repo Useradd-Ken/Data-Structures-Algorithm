7ab7436ac0a3c0d90ad4daf7deb79c5064215844
Kenrawr <20102188@usc.edu.ph>
2025-08-30 21:16:44 +0800

    DeleteAllOccurences()

--- C/Implementation/LinkedList/Delete.c
+++ C/Implementation/LinkedList/Delete.c
@@ -45,3 +45,36 @@ void deletePos(List *L,int pos){
     }
 }
 
+void deleteAllOccurences(List*L,int data){
+    List *trav;
+    for(trav=L;*trav!=NULL;){
+        if((*trav)->data == data){
+            List temp = *trav; //Store the address of the node to be deleted
+            *trav = (*trav)->next; //Bypass the node to be deleted
+            free(temp); //Free the memory of the deleted node
+        } else {
+            trav = &(*trav)->next; //Only move to next if we didn't delete the current node
+        }
+    }
+}
+
+void main()
+{
+    List L = NULL;//Initialize it to NULL before populating.
+    insertFirst(&L,10);
+    insertFirst(&L,20);
+    insertFirst(&L,30);
+     insertFirst(&L,30);
+      insertFirst(&L,30);
+       insertFirst(&L,30);
+    display(L);
+    deleteElement(&L,20); //Delete element 20
+    display(L);
+    deleteAllOccurences(&L,30); //Delete all occurences of 30
+    display(L);
+    deleteElement(&L,100); //Try to delete an element not in the list
+    deletePos(&L,2); //Delete element at position 2
+    display(L);
+    deletePos(&L,10); //Try to delete at a position out of bounds
+}
+
