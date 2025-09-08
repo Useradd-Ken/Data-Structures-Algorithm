d6c491762432a0cdbfaa990516f1d4ad5aed8206
Kenrawr <20102188@usc.edu.ph>
2025-09-08 23:41:26 +0800

    Array Implementation Definition

---
+++ C/Queues/CircularArray/CircularArray.h
@@ -0,0 +1,17 @@
+#ifndef CircularArray_h
+#define CircularArray_h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#define MAX 0xA10
+
+typedef struct{
+    int arr[MAX];
+    int front;
+    int rear;
+}Queues;
+
+
+
+#endif
