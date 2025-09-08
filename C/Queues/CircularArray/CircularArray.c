73c273af4bbdee49982ed8b0dc950272ad8abb6b
Kenrawr <20102188@usc.edu.ph>
2025-09-08 23:57:15 +0800

    enqueue()

--- C/Queues/CircularArray/CircularArray.c
+++ C/Queues/CircularArray/CircularArray.c
@@ -12,3 +12,15 @@ bool isFull(Queues Q){
 bool isEmpty(Queues Q){
     return Q.rear == -1;
 }
+
+void enqueue(Queues *Q,int data){
+    if(isFull(*Q)){
+        printf("Queue Overflow\n");
+        return;
+    }
+    Q->rear = (Q->rear + 1) % MAX;
+    Q->arr[Q->rear] = data;
+    if(Q->front == 0 && Q->rear == 0){
+        Q->front = 0; // If the first element is added, set front to 0
+    }
+}
