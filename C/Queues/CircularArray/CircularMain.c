c2378a85ac68cf357dc9e39b6824479ac5d4c00e
Kenrawr <20102188@usc.edu.ph>
2025-09-09 23:09:53 +0800

    Main()

--- C/Queues/CircularArray/CircularMain.c
+++ C/Queues/CircularArray/CircularMain.c
@@ -1,1 +1,13 @@
 #include "CircularArray.c"
+
+void main(){
+    Queues Q;
+    initQueue(&Q);
+    enqueue(&Q,10);
+    enqueue(&Q,20);
+    enqueue(&Q,30);
+    enqueue(&Q,40);
+    display(Q);
+    dequeue(&Q);
+    display(Q);
+}
