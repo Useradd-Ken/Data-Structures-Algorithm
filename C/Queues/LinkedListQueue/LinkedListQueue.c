e1584e26395f4f293a1201711491f54b3311225a
Kenrawr <20102188@usc.edu.ph>
2025-09-11 20:59:42 +0800

    Enqueue() & Dequeue()

---
+++ C/Queues/LinkedListQueue/LinkedListQueue.c
@@ -0,0 +1,30 @@
+#include "LinkedListQueue.h"
+
+void initQueue(Queue*Q){
+    Q->front = Q->rear = NULL;
+}
+
+void enqueue(Queue*Q,int data){
+    Nodeptr newNode = (Nodeptr)malloc(sizeof(struct node));
+    newNode->data = data;
+    newNode->next = NULL;
+    if(Q->front!=NULL){
+        Q->rear->next = newNode;
+        Q->rear = newNode;
+    }
+    else{
+        Q->front = Q->rear = newNode;
+    }
+}
+
+void dequeue(Queue*Q){
+    if(Q->front!=NULL){
+        Nodeptr temp = Q->front;
+        Q->front = temp->next;
+        free(temp);
+        if(Q->front==NULL){
+            Q->rear = NULL;
+        }
+    }
+}
+
