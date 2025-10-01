e17cd366c258fb06aafd1bb84413aac4d66c4d39
Kenrawr <20102188@usc.edu.ph>
2025-10-02 00:25:24 +0800

    Inheritance Code C#

---
+++ C#/Inheritance/Inheritance.cs
@@ -0,0 +1,32 @@
+using System;
+
+class Animal
+{
+    string breed;
+    int age;
+
+    public void setBreed(string Breed)
+    {
+        breed = Breed;
+    }
+    public string getBreed()
+    {
+        return breed;
+    }
+    public void setAge(int Age)
+    {
+        age = Age;
+    }
+    public int getAge()
+    {
+        return age;
+    }
+}
+
+class main
+{
+    static void Main()
+    {
+
+    }
+}
